925d3195fb7fb999afe1e7d9ecb36b971ac1e63d
lollomrt <lorenzomartini.mrt@outlook.it>
2023-03-28 17:44:03 +0200

    #2 close

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,21 @@
+<script>
+
+export default {
+    data() {
+        return {
+            value: ''
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="container-search d-flex align-items-center gap-3">
+        <input class="form-control" type="text" v-model="value" @keyup.enter="$emit('buttonClick', value)">
+        <button class="btn btn-primary" @click="$emit('buttonClick', value)"><i
+                class="fa-solid fa-magnifying-glass"></i></button>
+        <!-- <i class="fa-solid fa-user"></i> -->
+    </div>
+</template>
+<style lang="scss"></style>
