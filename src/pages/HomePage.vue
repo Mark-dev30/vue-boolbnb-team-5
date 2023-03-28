b0076c4be27cd0224870da140f679465675b7e6b
Mark-dev30 <fronterremarco@gmail.com>
2023-03-28 17:15:24 +0200

    Create apartmentlist, homepage

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'HomePage'
+}
+</script>
+<template lang="">
+    <div>
+        Homepage
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
---
+++ src/store.js
@@ -0,0 +1,4 @@
+import { reactive } from "vue";
+export const store = reactive({
+    apikey: 'sqAC6HGqUo0FuWA7iea7gmbV4KpA2wju'
+})
