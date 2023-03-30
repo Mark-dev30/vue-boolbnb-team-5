2575c85e83c1a2b948a76456b72d6b800998267c
Mark-dev30 <fronterremarco@gmail.com>
2023-03-30 17:05:27 +0200

    fixed apartment list

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -28,9 +28,10 @@ export default {
 <template>
     <div class="container-search d-flex align-items-center gap-3">
         <input class="form-control" type="text" v-model="value" @keyup.enter="$emit('buttonClick', getString(value))">
-        <button class="btn btn-primary" @click="$emit('buttonClick', getString(value))"><i
-                class="fa-solid fa-magnifying-glass"></i></button>
-        <!-- <i class="fa-solid fa-user"></i> -->
+        <router-link :to="{ name: 'apartment_list' }" class="btn btn-primary"
+            @click="$emit('buttonClick', getString(value))">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </router-link>
     </div>
 </template>
 <style lang="scss"></style>
