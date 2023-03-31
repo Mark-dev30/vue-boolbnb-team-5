b4cac9bc98a7cb174d7b79c9cfb0018602a83d8e
Aurora-Bizzo <aurora.bizzozero98@gmail.com>
2023-03-31 16:05:28 +0200

    single apartment completed

--- src/pages/SingleApartment.vue
+++ src/pages/SingleApartment.vue
@@ -11,16 +11,62 @@ export default {
         }
     },
     mounted() {
-        axios.get(`${this.store.baseUrl}/api/apartment/${this.$route.params.slug}`).then((response) => {
+        axios.get(`${store.baseUrl}/api/apartments/${this.$route.params.slug}`).then((response) => {
             this.apartment = response.data.apartment
-            console.log(this.apartment)
+            console.log(response)
         })
     }
 }
 </script>
 <template lang="">
-    <div>
-        ciao
+    <div class="container w-50 m-auto">
+        <div class="row">
+            <div class="col-12">
+                <div class="py-5 d-flex justify-content-center">
+                    <img :src="apartment.image != null ? `${this.store.baseUrl}/storage/${apartment.image}` : 'https://picsum.photos/300/200'" alt="" class="img-fluid h-17 rounded-4 w-100 h-auto">
+                </div>
+                <div class="">
+                    <h1>{{ apartment.title }}</h1>
+                </div>
+            </div>
+        </div>
+        <div class="row py-3">
+            <div class="col-3">
+                <div>
+                    PANORAMICA
+                </div>
+            </div>
+            <div class="col-9">
+                <div class="d-flex justify-content-between">
+                    <div>
+                        <i class="fa-solid fa-person-shelter"></i>
+                        <span class="mx-2"><strong>Stanze: </strong>{{ apartment.n_room }}</span>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-bed"></i>
+                        <span class="mx-2"><strong>Letti: </strong>{{ apartment.n_bed }}</span>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-bath"></i>
+                        <span class="mx-2"><strong>Bagni: </strong>{{ apartment.n_bathroom }}</span>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-kaaba"></i>
+                        <span class="mx-2"><strong>m&#178;: </strong>{{ apartment.mq }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row py-3">
+            <div class="col-3">
+                SERVIZI
+            </div>
+            <div class="col-9 d-flex justify-content-around">
+                <ul v-for="(service, index) in apartment.services" :key="index" class="list-unstyled">
+					<li><strong class="mx-2"><i :class="service.class_icon"></i></strong><strong>{{ service.name }}</strong></li>
+				</ul>
+            </div>
+        </div>
     </div>
 </template>
 
