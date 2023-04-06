6ef8997614480b9905540b75770f341f584e2558
Aurora-Bizzo <aurora.bizzozero98@gmail.com>
2023-04-06 17:52:06 +0200

    styling_complete

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,15 +1,20 @@
 <script>
 import axios from 'axios';
 import { store } from '../store';
+import Jumbotron from '../components/Jumbotron.vue';
+import Advertising from '../components/Advertising.vue';
 export default {
     name: 'HomePage',
     data() {
         return {
 
-
         }
     },
 
+    components: {
+        Jumbotron,
+        Advertising
+    },
 
     computed: {
 
@@ -24,9 +29,8 @@ export default {
 
 </script>
 <template lang="">
-    <div>
-       
-    </div>
+    <Jumbotron></Jumbotron>
+    <Advertising></Advertising>
 </template>
 
 <style lang="">
